566cbb3d94095b796ac45c3265c442d00924f4fb
Charlotte Stone <charlie.stone649@gmail.com>
2023-10-13 10:45:46 +0000

    Added error catching and main use of project

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,73 @@ const API_URL = "https://ci-jshint.herokuapp.com/api"
 const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"))
 
 document.getElementById("status").addEventListener("click", e => getStatus(e))
+document.getElementById("submit").addEventListener("click", e => postForm(e))
+
+
+/**
+ * For processing the information so that
+ * it send in a comma seperated list
+ */
+function processOptions(form){
+    let optArray = []
+
+    for (let entry of form.entries()) {
+        if(entry[0] === "options") {
+            optArray.push(entry[1])
+        }
+    }
+    form.delete("options")
+    form.append("options", optArray.join())
+    return form
+}
+
+/**
+ * For posting form data to API 
+ */
+async function postForm(e){
+    const form = processOptions(new FormData(document.getElementById("checksform")))
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+    },
+        body: form,
+   })
+   const data = await response.json()
+
+   if(response.ok){
+        displayErrors(data)
+   } else {
+        displayException(data)
+        throw new Error(data.error)
+   }
+}
+/**
+ * For formatting the response provided
+ * by the API
+ */
+function displayErrors(data){
+    
+    let results = ""
+    let heading = `JSHint Results for ${data.file}`
+
+    if (data.total_errors === 0) {
+        results = `<div class="no-errors>No errors reported!</div>`
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`
+        for (let error of data.error_list){
+            results += `<div>At line <span class="line">${error.line}</span>, `
+            results += `column <span class="column">${error.col}</span></div>`
+            results += `<div class="error">${error.error}</div>`
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading
+    document.getElementById("results-content").innerHTML = results
+    resultModal.show()
+}
+
 
 /**
  * Fetches the API using the custom key generated for me.
@@ -16,6 +83,7 @@ async function getStatus(e) {
     if(response.ok) {
         displayStatus(data)
     } else {
+        displayException(data)
         throw new Error(data.error)
     }
 }
@@ -28,6 +96,19 @@ function displayStatus(data){
     let heading = "API Key Status"
     let results = `<div>Your key is vaild until</div>`
     results += `<div class="key-staus">${data.expiry}</div>`
+
+    document.getElementById("resultsModalTitle").innerText = heading
+    document.getElementById("results-content").innerHTML = results
+
+    resultModal.show()
+}
+
+function displayException(data) {
+    let heading= `An Exception Occured`
+
+    results = `<div>The API returned status code ${data.status_code}</div>`
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`
+    results += `<div>Error text: <strong>${data.error}</strong></div>`
 
     document.getElementById("resultsModalTitle").innerText = heading
     document.getElementById("results-content").innerHTML = results
