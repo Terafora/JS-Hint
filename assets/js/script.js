36586126ea35865506d8e5f170708f78c6a04ca0
Charlotte Stone <charlie.stone649@gmail.com>
2023-10-13 09:04:25 +0000

    Added a function to bring up a modal which displays the fetched data on the API key status.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,33 @@ const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"))
 
 document.getElementById("status").addEventListener("click", e => getStatus(e))
 
+/**
+ * Fetches the API using the custom key generated for me.
+ * (Key is good until 13/10/2024)  
+ */
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`
     const response =await fetch(queryString)
     const data = await response.json()
 
     if(response.ok) {
-        console.log(data.expiry)
+        displayStatus(data)
+    } else {
+        throw new Error(data.error)
     }
+}
+
+/**
+ * Displays modal and populates it with data fetched
+ * from API
+ */
+function displayStatus(data){
+    let heading = "API Key Status"
+    let results = `<div>Your key is vaild until</div>`
+    results += `<div class="key-staus">${data.expiry}</div>`
+
+    document.getElementById("resultsModalTitle").innerText = heading
+    document.getElementById("results-content").innerHTML = results
+
+    resultModal.show()
 }
